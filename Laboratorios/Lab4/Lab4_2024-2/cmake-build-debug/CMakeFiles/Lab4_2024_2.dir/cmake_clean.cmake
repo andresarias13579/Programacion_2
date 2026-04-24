639c9ba6d4371c8713e135c38dee4e066c5b5961
andresarias13579 <143863922+andresarias13579@users.noreply.github.com>
2026-04-24 00:01:44 -0500

    Agregando carpeta compilada

---
+++ Laboratorios/Lab4/Lab4_2024-2/cmake-build-debug/CMakeFiles/Lab4_2024_2.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab4_2024_2.dir/Bibliotecas/FuncionesAuxiliares.cpp.obj"
+  "CMakeFiles/Lab4_2024_2.dir/Bibliotecas/FuncionesAuxiliares.cpp.obj.d"
+  "CMakeFiles/Lab4_2024_2.dir/main.cpp.obj"
+  "CMakeFiles/Lab4_2024_2.dir/main.cpp.obj.d"
+  "Lab4_2024_2.exe"
+  "Lab4_2024_2.exe.manifest"
+  "Lab4_2024_2.pdb"
+  "libLab4_2024_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab4_2024_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Laboratorios/Lab4/Lab4_2024-2/cmake-build-debug/CMakeFiles/Lab4_2024_2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Lab4_2024_2.
+# This may be replaced when dependencies are built.
